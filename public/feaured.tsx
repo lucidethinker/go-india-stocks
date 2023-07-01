64e4ed1a174a14b57551d87a4f9ad2798ba94cd8
Jasim Ihsan <jasimihsan2@gmail.com>
2023-07-01 16:37:48 +0530

    feat: :art: added featured companies

---
+++ public/feaured.tsx
@@ -0,0 +1,44 @@
+const featuredCompanies = [
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+    name: "Company 1",
+    description: "Description of Company 1",
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+    name: "Company 2",
+    description: "Description of Company 2",
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+    name: "Company 1",
+    description: "Description of Company 1",
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+    name: "Company 2",
+    description: "Description of Company 2",
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+    name: "Company 1",
+    description: "Description of Company 1",
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+    name: "Company 2",
+    description: "Description of Company 2",
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+    name: "Company 1",
+    description: "Description of Company 1",
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+    name: "Company 2",
+    description: "Description of Company 2",
+  },
+];
+
+export default featuredCompanies;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import Image from "next/image";
 import StockInfo from "./components/stockinfo";
+import featuredCompanies from "../../public/feaured";
 
 export default function Home() {
   return (
     <div className=" ">
       <StockInfo />
-      <div className=""></div>
+      <div className=" flex flex-col w-full   p-0 ">
+        <p className="mx-5 text-red-500 text-2xl font-bold">
+          FEATURED CONPANIES
+        </p>
+        <div className="flex gap-2 bg-[#87CEEB] p-10  ">
+          {/* Add your featured companies here */}
+          {featuredCompanies.map((company, index) => (
+            <div key={index} className="flex items-center  ">
+              <img src={company.logo} className="w-50 h-30 mr-4" />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
